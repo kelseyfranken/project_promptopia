0ff6f8ad3b1fd6ebea4cf38a078ef69391ec640e
Kelsey Franken <kcfranken@uc.cl>
2023-12-06 16:54:06 -0300

    chore: update package.json and database.js file

--- utils/database.js
+++ utils/database.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
     if (isConnected) {
         console.log('MongoDB is already connected');
         return;
